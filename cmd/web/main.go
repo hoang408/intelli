aeec05ef9712cb856da2cf8decd31c934d3d875c
nguyen.tnhoang@yahoo.com <nguyen.tnhoang@yahoo.com>
2023-07-21 23:01:19 +0700

    * re-arranged

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,13 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+const portNumber = ":8080"
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(portNumber, r)
 }
