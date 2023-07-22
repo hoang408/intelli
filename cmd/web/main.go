57fc9f226343140aed3cab52a2c92e1ddf00d1fa
nguyen.tnhoang@yahoo.com <nguyen.tnhoang@yahoo.com>
2023-07-22 15:20:56 +0700

    * link error

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -17,27 +18,59 @@ type TemplateData struct {
 	Processed string
 }
 
+type Student struct {
+	Raw       int
+	Processed string
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("page for GET")
-		w.Write([]byte("page for GET"))
+		// w.Write([]byte("page for GET"))
+		var tmpl_target = "index.tmpl"
+		// var tmpl_target = "../templates/index.tmpl"
+		// var tmpl_target = "../../templates/index.tmpl"
+		// var tmpl_target = "./templates/index.tmpl"
+		// var tmpl_target = "./templates/random.tmpl"
+		// entries, err := os.ReadDir("./templates/")
+		// entries, err := os.ReadDir("../../templates")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Println("Contents")
+		// for _, e := range entries {
+		// 	fmt.Println(e.Name())
+		// }
+
+		// tmpl, err := template.New("something").ParseFiles(tmpl_target)
+		// tmpl, err := template.New(tmpl_target).ParseFiles(tmpl_target)
+		// dat, err := os.ReadFile(tmpl_target)
+		tmpl, err := template.ParseFiles(tmpl_target)
+		if err != nil {
+			log.Println("Error parsing template: %v\n", tmpl_target)
+			log.Fatalln(err)
+		} else {
+			// td := TemplateData{Raw: "abc", Processed: "ABC"}
+			td := Student{Raw: 1, Processed: "ABC"}
+			tmpl.Execute(w, td)
+			// log.Println(dat)
+		}
 	})
+
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("page for POST")
+
+		// Acquire response from target URL
 		var url_dest = "https://developers.onemap.sg/commonapi/search?searchVal=revenue&returnGeom=n&getAddrDetails=n&pageNum=1"
 		resp, err := http.Get(url_dest)
-		defer resp.Body.Close()
 		if err != nil {
 			log.Printf("Error querying: %v\n", url_dest)
 			log.Fatalln(err)
 		} else {
 			fmt.Printf("Querying: %v\n", url_dest)
-			// fmt.Println(resp)
-
 			// Use the html package to parse the response body from the request
-			// doc, err := html.Parse(resp.Body)
 			resp_body, err := ioutil.ReadAll(resp.Body)
 			resp_body_str := string(resp_body)
 			if err != nil {
@@ -47,6 +80,21 @@ func main() {
 				w.Write([]byte("page for POST"))
 			}
 		}
+		defer resp.Body.Close()
 	})
 	http.ListenAndServe(portNumber, r)
 }
+
+// func indexHTMLTemplateVariableHandler(response http.ResponseWriter, request *http.Request) {
+// 	var tmpl_target = "./templates/random.tmpl"
+// 	tmplt := template.New(tmpl_target)       //create a new template with some name
+// 	tmplt, _ = tmplt.ParseFiles(tmpl_target) //parse some content and generate a template, which is an internal representation
+// 	p := Student{Raw: 1, Processed: "Aisha"} //define an instance with required field
+// 	tmplt.Execute(response, p)               //merge template ‘t’ with content of ‘p’
+// }
+
+// func main() {
+// 	fmt.Println("Starting Server for Templated response from file")
+// 	http.HandleFunc("/", indexHTMLTemplateVariableHandler)
+// 	http.ListenAndServe(portNumber, nil)
+// }
